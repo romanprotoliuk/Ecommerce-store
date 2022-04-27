79159ef0d05462ee8357cd012e977059a9d7c0aa
romanprotoliuk <romanprotoliuk@gmail.com>
2022-04-26 17:20:20 -0700

    redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,6 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
-
-
 const SignIn = () => {
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
     const userDocRef = await createUserDocumentFromAuth(user)
