6840d4ae6bf18c84ff617f7d2c63aaba59979d49
romanprotoliuk <romanprotoliuk@gmail.com>
2022-04-28 17:48:51 -0700

    added in our addToCart feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        {/* <Route path='checkout' element={<Authentication />} /> */}
       </Route>
     </Routes>
 
